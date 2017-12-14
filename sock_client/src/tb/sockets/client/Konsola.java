ba3c91e095aa34e5f2789126364da5ae14177a31
tb <tomasz.babczynski@pwr.edu.pl>
2017-12-14 18:49:32 +0100

    wywalone filteredoutput (eclipse wsadził)

--- sock_client/src/tb/sockets/client/Konsola.java
+++ sock_client/src/tb/sockets/client/Konsola.java
@@ -1,19 +1,18 @@
 package tb.sockets.client;
 
 import java.io.DataOutputStream;
-import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
 public class Konsola {
 
-	private static FilterOutputStream so;
+	private static DataOutputStream so;
 	private static Socket sock;
 
 	public static void main(String[] args) throws Exception {
 		try {
 			sock = new Socket("10.104.35.168", 6666);
-			DataOutputStream so = new DataOutputStream(sock.getOutputStream());
+			so = new DataOutputStream(sock.getOutputStream());
 			so.writeChars("wysyłam tekst\n");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
