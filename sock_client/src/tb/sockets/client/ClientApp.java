89e11ce3ec8b6eb4e0664bde33e3a3b42479be0a
michalkmiecik <michalkmiecik@bitbucket.org>
2018-01-03 22:56:10 +0100

    update

--- sock_client/src/tb/sockets/client/ClientApp.java
+++ sock_client/src/tb/sockets/client/ClientApp.java
@@ -27,27 +27,29 @@ import javax.swing.SwingConstants;
 public class ClientApp extends JFrame implements ActionListener {
 	private static final long serialVersionUID = -7165003859696110622L;
 
-	private String ipSerwera = "localhost";
+	private String ipSerwera = "172.16.24.211";
 
 	private JPanel contentPane;
 	private JPanel opponentsBattlefield = new JPanel(); // plansza przeciwnika - ta po lewej - ta którą odgaduję
-	private JButton[][] opponentsBattleFieldCells = new JButton[10][10]; // grid buttonów 10x10 na planszy przeciwnika
 	private JPanel myBattlefield = new JPanel(); // plansza moja - po prawej
+	private JButton[][] opponentsBattleFieldCells = new JButton[10][10]; // grid buttonów 10x10 na planszy przeciwnika
 	private JButton[][] myBattleFieldCells = new JButton[10][10]; // grid buttonów 10x10 na mojej planszy
-	private final JButton btnNieodkryte = new JButton("");
-	private final JButton btnPudlo = new JButton("");
-	private final JButton btnTrafiony = new JButton("");
-	private final JButton btnZatopiony = new JButton("");
-	private final JLabel lblNieodkryte = new JLabel("nieodkryte");
-	private final JLabel lblPudlo = new JLabel("pud\u0142o");
-	private final JLabel lblTrafiony = new JLabel("trafiony");
-	private final JLabel lblZatopiony = new JLabel("zatopiony");
-	private final JLabel lblPlanszaPrzeciwnika = new JLabel("PLANSZA PRZECIWNIKA");
-	private final JLabel lblPlanszaTwoja = new JLabel("PLANSZA TWOJA");
-	private final JButton btnStatek = new JButton("");
-	private final JButton btnMorze = new JButton("");
-	private final JLabel lblStatek = new JLabel("statek");
-	private final JLabel lblMorze = new JLabel("morze");
+	private JButton btnNieodkryte = new JButton("");
+	private JButton btnPudlo = new JButton("");
+	private JButton btnTrafiony = new JButton("");
+	private JButton btnZatopiony = new JButton("");
+	private JButton btnStatek = new JButton("");
+	private JButton btnMorze = new JButton("");
+	private JLabel lblNieodkryte = new JLabel("nieodkryte");
+	private JLabel lblPudlo = new JLabel("pud\u0142o");
+	private JLabel lblTrafiony = new JLabel("trafiony");
+	private JLabel lblZatopiony = new JLabel("zatopiony");
+	private JLabel lblPlanszaPrzeciwnika = new JLabel("PLANSZA PRZECIWNIKA");
+	private JLabel lblPlanszaTwoja = new JLabel("PLANSZA TWOJA");
+	private JLabel lblStatek = new JLabel("statek");
+	private JLabel lblMorze = new JLabel("morze");
+	private JLabel labelWhosTurn = new JLabel("");
+
 	private Socket socket;
 	private DataInputStream in;
 	private BufferedReader is;
@@ -55,18 +57,11 @@ public class ClientApp extends JFrame implements ActionListener {
 	private String myBoard = "";
 	private String shotCoordinates = "";
 	private boolean isItMyTurn;
+	private String responseLine;
 
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ClientApp frame = new ClientApp();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		ClientApp frame = new ClientApp();
+		frame.setVisible(true);
 	}
 
 	public ClientApp() {
@@ -92,6 +87,7 @@ public class ClientApp extends JFrame implements ActionListener {
 				}
 			}
 		});
+		gameStart();
 	}
 
 	private void connectToServer() throws UnknownHostException, IOException {
@@ -106,9 +102,151 @@ public class ClientApp extends JFrame implements ActionListener {
 		} else {
 			isItMyTurn = true;
 		}
-
 		myBoard = myBoard.substring(1);
 		System.out.println(myBoard);
+	}
+
+	private void gameStart() {
+		if (isItMyTurn) {
+			labelWhosTurn.setText("Your turn");
+		} else {
+			waitingForOpponentsMove();
+		}
+	}
+
+	private void setupOpponentsBattlefield() {
+		for (int i = 0; i < opponentsBattleFieldCells.length; i++) {
+			for (int j = 0; j < opponentsBattleFieldCells[0].length; j++) {
+				JButton tmp = new JButton();
+				tmp.addActionListener(this);
+				tmp.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
+				tmp.setBackground(Color.LIGHT_GRAY);
+				opponentsBattleFieldCells[i][j] = tmp;
+				opponentsBattlefield.add(opponentsBattleFieldCells[i][j]);
+			}
+		}
+	}
+
+	private void setupMyBattlefield() {
+		for (int i = 0; i < myBattleFieldCells.length; i++) {
+			for (int j = 0; j < myBattleFieldCells[0].length; j++) {
+				JButton tmp = new JButton();
+				tmp.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, 1));
+				tmp.setBorderPainted(true);
+				if (myBoard.charAt(i * myBattleFieldCells.length + j) == '1') {
+					tmp.setBackground(new Color(105, 105, 105));
+				} else {
+					tmp.setBackground(new Color(30, 144, 255));
+				}
+				tmp.setEnabled(false);
+				myBattleFieldCells[i][j] = tmp;
+				myBattlefield.add(myBattleFieldCells[i][j]);
+			}
+		}
+	}
+
+	public void waitingForOpponentsMove() {
+		// hang the turn, don't allow player to click and wait for server's info about
+		// opponents move
+		labelWhosTurn.setText("Waiting for opponent's turn");
+
+		try {
+			do {
+				responseLine = is.readLine();
+				System.out.println(responseLine);
+				processResponse(responseLine);
+			} while (responseLine.charAt(1) != 'm');
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		Object src = (JButton) e.getSource();
+		for (int i = 0; i < opponentsBattleFieldCells.length; i++) {
+			for (int j = 0; j < opponentsBattleFieldCells[0].length; j++) {
+				if (src == opponentsBattleFieldCells[i][j]) {
+					opponentsBattleFieldCells[i][j].setEnabled(false);
+					shotCoordinates = i + "" + j + "\n";
+					try {
+						os.writeBytes(shotCoordinates);
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+
+					try {
+						responseLine = is.readLine();
+						System.out.println(responseLine);
+						processResponse(responseLine);
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+
+	private void processResponse(String responseLine) {
+		if (responseLine.charAt(0) == '.') {
+			// game continues
+			if (isItMyTurn) {
+				// my turn
+				updateOpponentsBattleField(responseLine, shotCoordinates);
+				System.out.println("my turn");
+			} else {
+				// opponents turn
+				updateMyBattleField(responseLine, shotCoordinates);
+				System.out.println("opps turn");
+			}
+		} else if (responseLine.charAt(0) == 'L') {
+			// game is lost
+			JOptionPane.showMessageDialog(this, "You've lost the game :(");
+		} else if (responseLine.charAt(0) == 'W') {
+			// game is won
+			JOptionPane.showMessageDialog(this, "You've won the game :)");
+		}
+	}
+
+	private void updateOpponentsBattleField(String responseLine, String lastShotLocation) {
+		System.out.println(responseLine + "||" + lastShotLocation);
+		if (responseLine.charAt(1) == 'm') {
+			int x = lastShotLocation.charAt(0) - 48;
+			int y = lastShotLocation.charAt(1) - 48;
+			opponentsBattleFieldCells[x][y].setBackground(Color.WHITE);
+			isItMyTurn = !isItMyTurn;
+		} else if (responseLine.charAt(1) == 'h') {
+			int x = lastShotLocation.charAt(0) - 48;
+			int y = lastShotLocation.charAt(1) - 48;
+			opponentsBattleFieldCells[x][y].setBackground(Color.ORANGE);
+		} else if (responseLine.charAt(1) == 's') {
+			int lengthOfSunkenShip = (responseLine.length() - 2) / 2;
+			for (int i = 1; i <= lengthOfSunkenShip; i++) {
+				int x = responseLine.charAt(i * 2) - 48;
+				int y = responseLine.charAt(1 + i * 2) - 48;
+				opponentsBattleFieldCells[x][y].setBackground(Color.RED);
+			}
+		}
+	}
+
+	private void updateMyBattleField(String responseLine, String lastShotLocation) {
+		System.out.println(responseLine + "||" + lastShotLocation);
+		if (responseLine.charAt(1) == 'm') {
+			int x = lastShotLocation.charAt(0) - 48;
+			int y = lastShotLocation.charAt(1) - 48;
+			myBattleFieldCells[x][y].setBackground(Color.WHITE);
+			isItMyTurn = !isItMyTurn;
+		} else if (responseLine.charAt(1) == 'h') {
+			int x = lastShotLocation.charAt(0) - 48;
+			int y = lastShotLocation.charAt(1) - 48;
+			myBattleFieldCells[x][y].setBackground(Color.ORANGE);
+		} else if (responseLine.charAt(1) == 's') {
+			int lengthOfSunkenShip = (responseLine.length() - 2) / 2;
+			for (int i = 1; i <= lengthOfSunkenShip; i++) {
+				int x = responseLine.charAt(i * 2) - 48;
+				int y = responseLine.charAt(1 + i * 2) - 48;
+				myBattleFieldCells[x][y].setBackground(Color.RED);
+			}
+		}
 	}
 
 	private void drawGUI() {
@@ -199,124 +337,10 @@ public class ClientApp extends JFrame implements ActionListener {
 		lblMorze.setFont(new Font("Trebuchet MS", Font.PLAIN, 11));
 		lblMorze.setBounds(315, 538, 53, 14);
 		contentPane.add(lblMorze);
-	}
 
-	private void setupOpponentsBattlefield() {
-		for (int i = 0; i < opponentsBattleFieldCells.length; i++) {
-			for (int j = 0; j < opponentsBattleFieldCells[0].length; j++) {
-				JButton tmp = new JButton();
-				tmp.addActionListener(this);
-				tmp.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
-				tmp.setBackground(Color.LIGHT_GRAY);
-				opponentsBattleFieldCells[i][j] = tmp;
-				opponentsBattlefield.add(opponentsBattleFieldCells[i][j]);
-			}
-		}
-	}
-
-	private void setupMyBattlefield() {
-		for (int i = 0; i < myBattleFieldCells.length; i++) {
-			for (int j = 0; j < myBattleFieldCells[0].length; j++) {
-				JButton tmp = new JButton();
-				tmp.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, 1));
-				tmp.setBorderPainted(true);
-				if (myBoard.charAt(i * myBattleFieldCells.length + j) == '1') {
-					tmp.setBackground(new Color(105, 105, 105));
-				} else {
-					tmp.setBackground(new Color(30, 144, 255));
-				}
-				tmp.setEnabled(false);
-				myBattleFieldCells[i][j] = tmp;
-				myBattlefield.add(myBattleFieldCells[i][j]);
-			}
-		}
-	}
-
-	public void actionPerformed(ActionEvent e) {
-		Object src = (JButton) e.getSource();
-		for (int i = 0; i < opponentsBattleFieldCells.length; i++) {
-			for (int j = 0; j < opponentsBattleFieldCells[0].length; j++) {
-				if (src == opponentsBattleFieldCells[i][j]) {
-					opponentsBattleFieldCells[i][j].setEnabled(false);
-					shotCoordinates = i + "" + j + "\n";
-					try {
-						os.writeBytes(shotCoordinates);
-					} catch (IOException e1) {
-						e1.printStackTrace();
-					}
-
-					String responseLine;
-					try {
-						responseLine = is.readLine();
-						processResponse(responseLine);
-					} catch (IOException e1) {
-						e1.printStackTrace();
-					}
-
-				}
-			}
-		}
-	}
-
-	private void processResponse(String responseLine) {
-		if (responseLine.charAt(0) == '.') {
-			// game continues
-			if (isItMyTurn) {
-				// my turn
-				updateOpponentsBattleField(responseLine, shotCoordinates);
-			} else {
-				// opponents turn
-				updateMyBattleField(responseLine, shotCoordinates);
-			}
-		} else if (responseLine.charAt(0) == 'L') {
-			// game is lost
-			JOptionPane.showMessageDialog(this, "You've lost the game :(");
-		} else if (responseLine.charAt(0) == 'W') {
-			// game is won
-			JOptionPane.showMessageDialog(this, "You've won the game :)");
-		}
-		updateOpponentsBattleField(responseLine, shotCoordinates);
-	}
-
-	private void updateOpponentsBattleField(String responseLine, String lastShotLocation) {
-		System.out.println(responseLine + "||" + lastShotLocation);
-		if (responseLine.charAt(1) == 'm') {
-			// zablokuj gre i wyswietl komunikat o czekaniu
-			int x = lastShotLocation.charAt(0) - 48;
-			int y = lastShotLocation.charAt(1) - 48;
-			opponentsBattleFieldCells[x][y].setBackground(Color.WHITE);
-		} else if (responseLine.charAt(1) == 'h') {
-			int x = lastShotLocation.charAt(0) - 48;
-			int y = lastShotLocation.charAt(1) - 48;
-			opponentsBattleFieldCells[x][y].setBackground(Color.ORANGE);
-		} else if (responseLine.charAt(1) == 's') {
-			int lengthOfSunkenShip = (responseLine.length() - 2) / 2;
-			for (int i = 1; i <= lengthOfSunkenShip; i++) {
-				int x = responseLine.charAt(i * 2) - 48;
-				int y = responseLine.charAt(1 + i * 2) - 48;
-				opponentsBattleFieldCells[x][y].setBackground(Color.RED);
-			}
-		}
-	}
-
-	private void updateMyBattleField(String responseLine, String lastShotLocation) {
-		System.out.println(responseLine + "||" + lastShotLocation);
-		if (responseLine.charAt(1) == 'm') {
-			// zablokuj gre i wyswietl komunikat o czekaniu
-			int x = lastShotLocation.charAt(0) - 48;
-			int y = lastShotLocation.charAt(1) - 48;
-			opponentsBattleFieldCells[x][y].setBackground(Color.WHITE);
-		} else if (responseLine.charAt(1) == 'h') {
-			int x = lastShotLocation.charAt(0) - 48;
-			int y = lastShotLocation.charAt(1) - 48;
-			opponentsBattleFieldCells[x][y].setBackground(Color.ORANGE);
-		} else if (responseLine.charAt(1) == 's') {
-			int lengthOfSunkenShip = (responseLine.length() - 2) / 2;
-			for (int i = 1; i <= lengthOfSunkenShip; i++) {
-				int x = responseLine.charAt(i * 2) - 48;
-				int y = responseLine.charAt(1 + i * 2) - 48;
-				opponentsBattleFieldCells[x][y].setBackground(Color.RED);
-			}
-		}
+		labelWhosTurn.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		labelWhosTurn.setHorizontalAlignment(SwingConstants.CENTER);
+		labelWhosTurn.setBounds(500, 491, 400, 69);
+		contentPane.add(labelWhosTurn);
 	}
 }
--- sock_serwer/src/tb/sockets/server/Board.java
+++ sock_serwer/src/tb/sockets/server/Board.java
@@ -5,138 +5,121 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
-    private static int boardLengthX;
-    private static int boardLengthY;
+	private static int boardLengthX;
+	private static int boardLengthY;
 
-    private List<Battleship> battleships = new ArrayList<>();
-    private static final int[] shipLengths = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
+	private List<Battleship> battleships = new ArrayList<>();
+	private static final int[] shipLengths = { 4, 3, 3, 2, 2, 2, 1, 1, 1, 1 };
 
-    public Board(int boardLengthX, int boardLengthY) {
-        this.boardLengthX = boardLengthX;
-        this.boardLengthY = boardLengthY;
-        this.addShips();
-    }
+	public Board(int boardLengthX, int boardLengthY) {
+		this.boardLengthX = boardLengthX;
+		this.boardLengthY = boardLengthY;
+		this.addShips();
+	}
 
-    //TODO: fix generating ships
-    private void addShips() {
-        for (int length : shipLengths) {
-            boolean added = false;
-            Battleship battleship = new Battleship(length);
-            while (!added) {
-                int x = (int) (boardLengthX * Math.random());
-                int y = (int) (boardLengthY * Math.random());
-                boolean horizontal = ((int) (10 * Math.random())) % 2 == 0;
-                if (horizontal) {
-                    // Check for vertical space
-                    boolean hasSpace = true;
-                    for (int i = 0; i < length; i++) {
-                        if (y + i >= boardLengthY) {
-                            hasSpace = false;
-                            break;
-                        }
-                        if (checkIfCoordinatesUsed(x, y+i)) {
-                        hasSpace = false;
-                        break;
-                        }
-                    }
-                    if (!hasSpace) {
-                        // No room there, check again
-                        continue;
-                    }
-                    for (int i = 0; i < length; i++) {
-                        //System.out.println(new Point(x, y + i));
-                        battleship.addCoordinate(new Point(x, y + i));
-                    }
-                    added = true;
-                } else {
-                    // Check for horizontal space
-                    boolean hasSpace = true;
-                    for (int i = 0; i < length; i++) {
-                        if (x + i >= boardLengthX) {
-                            hasSpace = false;
-                            break;
-                        }
-                        if(checkIfCoordinatesUsed(x+i,y))
-                        {
-                            hasSpace = false;
-                            break;
-                        }
-                    }
-                    if (!hasSpace) {
-                        // No room there, check again
-                        continue;
-                    }
-                    for (int i = 0; i < length; i++) {
-                        //System.out.println(new Point(x + i, y));
-                        battleship.addCoordinate(new Point(x + i, y));
-                    }
-                    added = true;
-                }
-            }
-            battleships.add(battleship);
-        }
-    }
+	// TODO: fix generating ships
+	private void addShips() {
+		for (int length : shipLengths) {
+			boolean added = false;
+			Battleship battleship = new Battleship(length);
+			while (!added) {
+				int x = (int) (boardLengthX * Math.random());
+				int y = (int) (boardLengthY * Math.random());
+				boolean horizontal = ((int) (10 * Math.random())) % 2 == 0;
+				if (horizontal) {
+					// Check for vertical space
+					boolean hasSpace = true;
+					for (int i = 0; i < length; i++) {
+						if (y + i >= boardLengthY) {
+							hasSpace = false;
+							break;
+						}
+						if (checkIfCoordinatesUsed(x, y + i)) {
+							hasSpace = false;
+							break;
+						}
+					}
+					if (!hasSpace) {
+						// No room there, check again
+						continue;
+					}
+					for (int i = 0; i < length; i++) {
+						// System.out.println(new Point(x, y + i));
+						battleship.addCoordinate(new Point(x, y + i));
+					}
+					added = true;
+				} else {
+					// Check for horizontal space
+					boolean hasSpace = true;
+					for (int i = 0; i < length; i++) {
+						if (x + i >= boardLengthX) {
+							hasSpace = false;
+							break;
+						}
+						if (checkIfCoordinatesUsed(x + i, y)) {
+							hasSpace = false;
+							break;
+						}
+					}
+					if (!hasSpace) {
+						// No room there, check again
+						continue;
+					}
+					for (int i = 0; i < length; i++) {
+						// System.out.println(new Point(x + i, y));
+						battleship.addCoordinate(new Point(x + i, y));
+					}
+					added = true;
+				}
+			}
+			battleships.add(battleship);
+		}
+	}
 
-    private boolean checkIfCoordinatesUsed(int x, int y)
-    {
-        for(Battleship battleship: battleships)
-        {
-            if(battleship.getCoordinates().contains(new Point(x,y)))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
+	private boolean checkIfCoordinatesUsed(int x, int y) {
+		for (Battleship battleship : battleships) {
+			if (battleship.getCoordinates().contains(new Point(x, y))) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    public String getBoardAsString() {
-        String boardAsString = "";
-        for(int i = 0; i < boardLengthX; i++)
-        {
-            for (int j = 0; j < boardLengthY; j++)
-            {
-                boardAsString += (checkIfCoordinatesUsed(i,j)? "1" : "0");
-            }
-        }
+	public String getBoardAsString() {
+		String boardAsString = "";
+		for (int i = 0; i < boardLengthX; i++) {
+			for (int j = 0; j < boardLengthY; j++) {
+				boardAsString += (checkIfCoordinatesUsed(i, j) ? "1" : "0");
+			}
+		}
 
-        //test of the generated string
-/*        for(int i = 0; i < 100; i++)
-        {
-            if(i % 10 == 0){
-                System.out.print("\n");
-            }
-            System.out.print(boardAsString.charAt(i));
-        }
-            System.out.print('\n');*/
-        return boardAsString;
-    }
+		// test of the generated string
+		/*
+		 * for(int i = 0; i < 100; i++) { if(i % 10 == 0){ System.out.print("\n"); }
+		 * System.out.print(boardAsString.charAt(i)); } System.out.print('\n');
+		 */
+		return boardAsString;
+	}
 
-    public String getIfShipMissedHitOrSunken(int x, int y)
-    {
-        for(Battleship battleship: battleships)
-        {
-            if(battleship.getCoordinates().contains(new Point(x,y)))
-            {
-                battleship.addHit();
-                if(battleship.checkIfSunken())
-                {
-                    return "s" + battleship.getCoordinatesAsString();
-                }
-                return "h";
-            }
-        }
-        return "m";
-    }
+	public String getIfShipMissedHitOrSunken(int x, int y) {
+		for (Battleship battleship : battleships) {
+			if (battleship.getCoordinates().contains(new Point(x, y))) {
+				battleship.addHit();
+				if (battleship.checkIfSunken()) {
+					return "s" + battleship.getCoordinatesAsString();
+				}
+				return "h";
+			}
+		}
+		return "m";
+	}
 
-    public boolean checkIfAllShipsSunken()
-    {
-        for(Battleship battleship: battleships)
-        {
-            if(battleship.checkIfSunken() == false)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
+	public boolean checkIfAllShipsSunken() {
+		for (Battleship battleship : battleships) {
+			if (battleship.checkIfSunken() == false) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
